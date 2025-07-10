9f895a39e124cf5b08eae2545afc7af2a4291f80
MaNmAxIm0 <luisfmaximo8@gmail.com>
2025-07-10 12:42:15 +0100

    New

--- _includes/data/site.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // O prefixo do caminho para o seu site no GitHub Pages.
-  // Se o seu repositório é https://github.com/manmaxim0/Bia, o prefixo é "/Bia".
-  // Se você estivesse a usar um domínio personalizado, isto seria uma string vazia "".
-  pathPrefix: "/Bia"
-};
